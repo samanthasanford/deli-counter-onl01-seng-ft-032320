91e7e11f9835af35abc34200873e8bf131213ae8
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 01:14:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,20 @@ def line(katz_deli)
    
  end
  
-   
   
 end 
 
 
+def take_a_number(katz_deli, name)
+  
+  if katz_deli.length == 0 
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+    
+  else katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+    
+  end
+end
 
 
