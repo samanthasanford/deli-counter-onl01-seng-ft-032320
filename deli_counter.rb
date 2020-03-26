fb4fd1e433b83396cc460a7bd8f78dd76e218487
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 00:55:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+    
+  else 
+    katz_deli >= 1 
+  puts "The line is currently empty."
+  
+
+  
+end 
+
+
+
+
